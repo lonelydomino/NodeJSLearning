69357ca7954207f5f5ba8a3f6beb17d77f8de750
Milo <zeroeclipse@live.com>
2022-04-04 18:07:00 -0700

    Updating.

--- models/product.js
+++ models/product.js
@@ -1,32 +1,33 @@
 const fs = require('fs')
 const path = require('path')
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+
+const getProductsFromFile = (cb) =>{
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([])
+        }
+        cb(JSON.parse(fileContent))
+    })
+}
 module.exports = class Product {
-    constructor(title) {
+    constructor(title, imageUrl, description, price) {
         this.title = title
+        this.imageUrl = imageUrl
+        this.description = description
+        this.price = price
     }
 
     save() {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
-        fs.readFile(p, (err, fileContent) => {
-            let products = []
-            if(!err) {
-                products = JSON.parse(fileContent)
-            }
+        getProductsFromFile( products => { 
             products.push(this)
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err)
             })
         })
-    }
+        }
 
     static fetchAll(cb) {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
-
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([])
-            }
-            cb(JSON.parse(fileContent))
-        })
+        getProductsFromFile(cb)
     }
 }
