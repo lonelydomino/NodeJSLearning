51ac687c04bc32a6697046df9e54a201e77708a9
Milo <zeroeclipse@live.com>
2022-04-08 21:52:40 -0700

    Updating project.

--- app.js
+++ app.js
@@ -2,20 +2,18 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const errorController = require('./controllers/404')
-// const expressHbs = require('express-handlebars')//Handlebars setup
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-// app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main'}))//Handlebars setup
-// app.set('view engine', 'pug')//Pug setup
-app.set('view engine', 'ejs')//set engine here
-app.set('views', 'views')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
